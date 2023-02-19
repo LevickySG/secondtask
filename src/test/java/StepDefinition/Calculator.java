3fb1cd16205cc65b2924c8d05d1f68d965c4fb16
LevickySG <levickysg@gmail.com>
2023-02-19 21:12:41 +0400

    4rd task Rebuild initial values

--- src/test/java/StepDefinition/Calculator.java
+++ src/test/java/StepDefinition/Calculator.java
@@ -1,30 +1,21 @@
 package StepDefinition;
 
-import io.cucumber.java.ru.Затем;
-import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.Тогда;
 
-import java.util.Scanner;
-
 public class Calculator {
-    @Когда("^Пользователь вводит x$")
-    public Double Пользователь_вводит_x() {
-        Double x = 10.0;
-        return x;
+    public static double x;
+    public static double y;
+    @Дано("число {double} и число {double}")
+    public static void число_и_число(Double numx, Double numy){
+        x = numx;
+        y = numy;
     }
-    @Когда("^Пользователь вводит y$")
-    public Double Пользователь_вводит_y(){
-        Double y = 20.0;
-        return y;
-    }
-    @Тогда("^Получить сумму$")
-    public Double Получить_сумму(){
-        return (Пользователь_вводит_x() + Пользователь_вводит_y());
-    }
-    @Затем("^Вывести результат$")
+
+    @Тогда("^Вывести результат$")
     public void Вывести_результат(){
-        System.out.println("x=" + Пользователь_вводит_x());
-        System.out.println("y=" + Пользователь_вводит_y());
-        System.out.println("x+y=" + Получить_сумму());
+        System.out.println("x=" + x);
+        System.out.println("y=" + y);
+        System.out.println("x+y=" + (x+y));
     }
 }
