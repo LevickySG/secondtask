97ce6ac0697a2de379e8e17500a7c311285864aa
LevickySG <levickysg@gmail.com>
2023-02-27 03:51:41 +0400

    5.3 task

--- src/test/java/TestAPI.java
+++ src/test/java/TestAPI.java
@@ -1,17 +1,10 @@
 import APISteps.API;
 import org.junit.Test;
 
-import static APISteps.API.*;
 
 public class TestAPI {
     @Test
-    public void test1(){
-        charID = "2";
-        API.getCharacter("2");
-        API.getLastEpisode("2");
-        API.getCharFromEpisode(LastEpisodeId);
-        API.getCharSpecies(LastCharacterId);
-        API.getCharLocation(LastCharacterId);
-        API.dataEqual("2",LastCharacterId);
+    public void test1() {
+        API.authen();
     }
 }
